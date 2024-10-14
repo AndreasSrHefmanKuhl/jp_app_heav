dc779c975ec7b38074288916b5cca9c4fd711c70
AndreasKuhl <hefbolzen@gmail.com>
2024-10-14 14:39:06 +0200

    cleaned main+ setted homescreen

--- jp_app_heav/lib/Features/home/screens/home_screen.dart
+++ jp_app_heav/lib/Features/home/screens/home_screen.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:jp_app_heav/main.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _MyHomeScreenState();
+}
+class _MyHomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/hintergründe/bg_mainscreen.png'),
+                    fit: BoxFit.cover)),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card()
+  }
 }
--- jp_app_heav/lib/main.dart
+++ jp_app_heav/lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jp_app_heav/features/home/screens/home_screen.dart';
 
 void main() {
   runApp(const HomeScreen());
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _MyHomeScreenState();
-}
-
-class _MyHomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/hintergründe/bg_mainscreen.png'),
-                    fit: BoxFit.cover)),
-          ),
-        ]),
-      ),
-    );
-  }
 }
