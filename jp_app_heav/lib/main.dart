2004b161e2aeb3976eec5a156731ea31344d1a30
AndreasKuhl <hefbolzen@gmail.com>
2024-10-14 12:34:21 +0200

    cleaned

--- jp_app_heav/lib/main.dart
+++ jp_app_heav/lib/main.dart
@@ -4,26 +4,3 @@ import 'package:jp_app_heav/Features/Screens/my_startscreen.dart';
 void main() {
   runApp(const MyStartscreen());
 }
-
-class _MyStartscreenState extends State<MyStartscreen> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('assets/images/hintergründe/bg_startscreen.png'),
-          fit: BoxFit.cover,
-        )),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/grafiken/chick cupcakes_3D.png'),
-            Image.asset('assets/images/details/T2.png'),
-          ],
-        ),
-      )),
-    );
-  }
-}
