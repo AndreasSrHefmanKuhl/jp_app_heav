19086f789f719d689ab4f85e3966967628bd76d5
AndreasKuhl <hefbolzen@gmail.com>
2024-10-14 16:52:21 +0200

    carved on screen

--- jp_app_heav/lib/Features/home/screens/home_screen.dart
+++ jp_app_heav/lib/Features/home/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/src/widgets/scroll_physics.dart';
+import 'package:jp_app_heav/features/home/widgets/tab_bar_container.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,6 +8,7 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _MyHomeScreenState();
 }
+
 class _MyHomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
@@ -15,21 +17,42 @@ class _MyHomeScreenState extends State<HomeScreen> {
         body: Stack(children: [
           Container(
             decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/hintergründe/bg_mainscreen.png'),
-                    fit: BoxFit.cover)),
+              image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/hintergründe/bg_mainscreen.png'),
+                  fit: BoxFit.cover),
+            ),
           ),
+          const Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            SizedBox(height: 70),
+            Text(
+              'choose your favorite snack!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.all(9.0),
+              child: SingleChildScrollView(
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  TabBarContainer(text: 'Salty'),
+                  TabBarContainer(text: 'sweety'),
+                  TabBarContainer(text: 'spicy')
+                ]),
+              ),
+            ),
+          ]),
         ]),
       ),
     );
   }
 }
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card()
+    return const Card();
   }
 }
