03a798104323cb80e377282a6025c612ed9095ae
AndreasKuhl <hefbolzen@gmail.com>
2024-10-15 14:01:06 +0200

    made data

---
+++ jp_app_heav/lib/Features/home/repository/yummy_card_data.dart
@@ -0,0 +1,25 @@
+import 'package:jp_app_heav/features/home/widgets/yummy_card.dart';
+
+List<YummyCard> products = const [
+  YummyCard(
+    assets: 'assets/images/grafiken/Burger_3D.png',
+    title: 'Angis Yummy Burger',
+    description: 'Delish vergan burger that testes like heaven',
+    price: '₳ 13.99',
+    value: '120',
+  ),
+  YummyCard(
+    assets: 'assets/images/grafiken/cat_cupcake_3D.png',
+    title: 'Moglis Cup',
+    description: 'Strawberry ice cream',
+    price: '₳ 8.99',
+    value: '200',
+  ),
+  YummyCard(
+    assets: 'assets/iamges/grafiken/ice.cream.png',
+    title: 'Balu`s Cup',
+    description: 'Pistachio ice cream',
+    price: '₳ 8.99',
+    value: '100',
+  ),
+];
