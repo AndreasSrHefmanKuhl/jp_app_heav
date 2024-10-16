8ec31b9031f8547dcd22cccf972eba91c395068f
AndreasKuhl <hefbolzen@gmail.com>
2024-10-16 20:23:52 +0200

    finished card

--- jp_app_heav/lib/Features/home/repository/yummy_card_data.dart
+++ jp_app_heav/lib/Features/home/repository/yummy_card_data.dart
@@ -1,22 +1,19 @@
-import 'package:jp_app_heav/features/home/widgets/yummy_card.dart';
+import 'package:jp_app_heav/features/home/widgets/slide_card.dart';
 
-List<YummyCard> products = const [
-  YummyCard(
-    assets: 'assets/images/grafiken/Burger_3D.png',
-    title: 'Angis Yummy Burger',
-    description: 'Delish vergan burger that testes like heaven',
-    price: '₳ 13.99',
-  ),
-  YummyCard(
-    assets: 'assets/images/grafiken/cat_cupcake_3D.png',
-    title: 'Moglis Cup',
-    description: 'Strawberry ice cream',
-    price: '₳ 8.99',
-  ),
-  YummyCard(
-    assets: 'assets/iamges/grafiken/ice.cream.png',
-    title: 'Balu`s Cup',
-    description: 'Pistachio ice cream',
-    price: '₳ 8.99',
-  ),
+List<SlideProduct> products = const [
+  SlideProduct(
+      asset: 'assets/images/grafiken/Burger_3D.png',
+      title: 'Angis Yummy Burger',
+      description: 'Delish vergan burger that testes like heaven',
+      price: '₳ 13.99'),
+  SlideProduct(
+      asset: 'assets/images/grafiken/cat_cupcake_3D.png',
+      title: 'Moglis Cup',
+      description: 'Strawberry ice cream',
+      price: '₳ 8.99'),
+  SlideProduct(
+      asset: 'assets/iamges/grafiken/ice.cream.png',
+      title: 'Balu`s Cup',
+      description: 'Pistachio ice cream',
+      price: ' ₳ 8.99'),
 ];
