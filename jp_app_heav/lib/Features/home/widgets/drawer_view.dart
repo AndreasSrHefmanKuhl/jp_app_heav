cd861b9c13cbb09ca87199b2d89ed035ca645f36
AndreasKuhl <hefbolzen@gmail.com>
2024-10-16 20:24:40 +0200

    made scrollview

---
+++ jp_app_heav/lib/Features/home/widgets/drawer_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DrawerView extends StatelessWidget {
+  const DrawerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(218, 168, 112, 225),
+                  Color.fromARGB(255, 200, 122, 255)
+                ],
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+              ),
+            ),
+            child: Text(
+              'Food Menu',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+          _buildDrawerItem(context, Icons.icecream, "Mogli's Cup"),
+          _buildDrawerItem(context, Icons.icecream, "Balu's Cup"),
+          _buildDrawerItem(context, Icons.icecream, "Ice Cream Stick"),
+          _buildDrawerItem(context, Icons.icecream, "Ice Cup"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem(BuildContext context, IconData icon, String title) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+}
