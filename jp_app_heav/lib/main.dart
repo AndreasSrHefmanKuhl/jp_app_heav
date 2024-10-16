7935069105cc204b8cfc549b913e878e7c4d53b1
AndreasKuhl <hefbolzen@gmail.com>
2024-10-16 18:02:01 +0200

    .

--- jp_app_heav/lib/main.dart
+++ jp_app_heav/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:jp_app_heav/features/home/screens/home_screen.dart';
+
+import 'package:jp_app_heav/features/start/screens/my_startscreen.dart';
 
 void main() {
-  runApp(const HomeScreen());
+  runApp(const MyStartscreen());
 }
