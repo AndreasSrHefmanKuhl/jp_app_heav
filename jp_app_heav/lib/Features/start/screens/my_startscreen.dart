374975701ab66a41e486369a94d55d370e3c431d
AndreasKuhl <hefbolzen@gmail.com>
2024-10-16 18:01:55 +0200

    startScreen fin

--- jp_app_heav/lib/Features/start/screens/my_startscreen.dart
+++ jp_app_heav/lib/Features/start/screens/my_startscreen.dart
@@ -1,53 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:jp_app_heav/features/home/screens/home_screen.dart';
 import 'package:jp_app_heav/features/start/widget/snackish_box.dart';
 
-class MyStartscreen extends StatefulWidget {
+class MyStartscreen extends StatelessWidget {
   const MyStartscreen({super.key});
 
-  @override
-  State<MyStartscreen> createState() => _MyStartscreenState();
-}
-
-class _MyStartscreenState extends State<MyStartscreen> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Stack(children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image:
-                    AssetImage('assets/images/hintergründe/bg_startscreen.png'),
-                fit: BoxFit.cover,
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset("assets/images/hintergründe/bg_startscreen.png",
+                fit: BoxFit.cover),
+            Positioned(
+                top: 242,
+                left: 115,
+                child: Transform.scale(
+                    scale: 2.0,
+                    child: Image.asset(
+                        "assets/images/grafiken/chick cupcakes_3D.png"))),
+            Positioned(
+                left: 0,
+                right: 0,
+                bottom: 60,
+                child: Image.asset("assets/images/details/T2.png",
+                    fit: BoxFit.fitWidth)), // Bottom detail image
+            Positioned(
+              bottom: 90,
+              left: 30,
+              right: 30,
+              child: SnackishBox(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomeScreen()),
+                  );
+                },
               ),
             ),
-          ),
-          Positioned(
-            bottom: 175.0,
-            right: -140,
-            child: Image.asset(
-              'assets/images/grafiken/chick cupcakes_3D.png',
-              scale: 0.442,
-            ),
-          ),
-          Positioned(
-            bottom: 65,
-            right: 0,
-            child: Image.asset(
-              'assets/images/details/T2.png',
-              scale: 1,
-              colorBlendMode: BlendMode.difference,
-            ),
-          ),
-          // ignore: prefer_const_constructors
-          Positioned(
-            bottom: 50,
-            right: 30,
-            left: 30,
-            child: const SnackishBox(),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
